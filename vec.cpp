f2dd825259cd42ad54695f18df56d0d29d65d1f1
kolaczyn <65826144+kolaczyn@users.noreply.github.com>
2020-06-14 10:50:01 +0200

    Create vec.cpp

---
+++ vec.cpp
@@ -0,0 +1,53 @@
+#include "vec.h"
+
+template<typename T, int n>
+vec<T, n>::vec() {
+	for (int i = 0; i < n; i++)
+		(*this)[i] = 0;
+}
+
+template<typename T, int n>
+vec<T, n>::vec(T init) {
+	for (int i = 0; i < n; i++)
+		(*this)[i] = init;
+}
+
+template<typename T, int n>
+vec<T, n>::~vec() {}
+
+template<typename T, int n>
+vec<T, n> vec<T, n>::operator+(const vec& other) {
+	vec out;
+	for (int i = 0; i < n; i++)
+		out[i] = this->arr[i] + other.arr[i];
+	return out;
+}
+
+template<typename T, int n>
+vec<T, n> vec<T, n>::operator-(const vec& other) {
+	vec out;
+	for (int i = 0; i < n; i++)
+		out[i] = this->arr[i] - other.arr[i];
+	return out;
+}
+
+template<typename T, int n>
+T vec<T, n>::dot(const vec& other) {
+	T out = 0;
+	for (int i = 0; i < n; i++)
+		out += (*this)[i] * other[i];
+	return out;
+}
+
+template<typename T, int n>
+T vec<T, n>::operator[](int i) const {
+	return arr[i];
+}
+template<typename T, int n>
+ T& vec<T, n>::operator[](int i) {
+	return arr[i];
+}
+
+template class vec<float, 2>;
+template class vec<float, 3>;
+template class vec<float, 4>;
